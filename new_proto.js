5ca2d6c91a32a1fd2020e4cf8b00191b24f6e9b6
JGT <john.thevos@gmail.com>
2015-04-21 17:21:22 -0400

    source javascript logic

---
+++ new_proto.js
@@ -0,0 +1,202 @@
+/* Coded by John G Thevos */
+
+// tracking asyncs
+var async_count = 0;
+var has_processes = false;
+var done = false;
+
+var tab_count_derived = 0;
+
+// Master Tabs array - to be populated with JSON objects
+var Tabs = new Array();
+var Process_Array = new Array();  // capitalized because it will be the master array of objects.
+
+
+// HTML Handlers
+var bodyNode = document.getElementsByTagName("body");
+var olNode = document.getElementsByTagName("ol");
+
+
+// execute
+testListener(); 
+extractInfo(Process_Array);
+
+getRemainingMemory();
+
+
+// Function List
+
+function testListener() {
+
+	chrome.processes.onUpdatedWithMemory.addListener(function(procs) {
+/* This adds an event listener but as far as I can tell, there's 
+no way to remove it. As such it will keep executing the callback 
+function approx every second, starting the whole cascade again.
+I get around this by using a boolean to see if I have one and only one
+set of processes. Now, I only execute my series of callbacks once, thus 
+giving me the "snapshot" I'm looking for. */
+
+		if (has_processes === false) {
+			has_processes = true;
+
+			console.dir(procs);
+			console.debug("Check 1");
+
+/* This is my way of "drilling down". procs is a dictionary of process objects, and once i have
+those objects, I need to analyze them and see what kind of process it is. I learned from debugging 
+that procs[key] always == "[object Object]". I use this to drill down to the objects themselves. */
+
+			for (var key in procs) {
+				if (procs.hasOwnProperty(key)) {
+					if (procs[key] == "[object Object]") { 
+						extractInfo(procs[key]); // get all relevant information
+					}
+				}
+			}
+		}
+	});
+}
+
+function extractInfo(object_index) {
+
+//console.debug("extractInfo Test");
+	if (object_index.type == "renderer") {
+
+		constructObject(object_index.osProcessId, 
+						object_index.id, 
+						clipTitle(object_index.title), 
+						object_index.jsMemoryAllocated);
+		async_count++;
+
+		//console.debug("extractInfo Test 2");
+
+	}
+	if (Tabs.length == async_count) {
+		//insertionSort(Tabs);
+	}
+	console.dir(Tabs);
+}
+
+
+
+	// apply urls separately bc they are synchronous calls
+
+
+function clipTitle(title) {
+// because I'm getting the titles from the task manager, all tabs will start with "Tab: "
+// The substring() function solves that for me.
+	var old_title = title;
+	var new_title = old_title.substring(5);
+	return new_title;
+}
+					
+
+function constructObject(proc,tab,titl,mem) {
+	Tabs.push({"proc_id": proc, "tab_id": tab, "tab_title": titl, "url": "", "allocd_mem": mem});
+}
+
+function insertionSort(obj_array) {
+
+    var value, i, j;                    // the value currently being compared   
+    for (i=0; i < obj_array.length; i++) {
+        value = obj_array[i].allocd_mem;
+        for (j=i-1; j > -1 && obj_array[j].allocd_mem > value; j--) {
+            obj_array[j+1] = obj_array[j];
+        }
+        obj_array[j+1] = value;
+    }
+    return obj_array;
+}
+
+function getUrls(tabs) {
+	//get URL from tab ID
+}
+
+function kill(tab_Id) {
+	chrome.processes.terminate(tab_Id, function(didTerminate) {
+		if (didTerminate) {
+			console.debug("termination of " + tab_Id + " successful.");
+			// has process = false; to restart the listener
+		}
+	});
+}
+
+/*
+function favorite(tab_Id, tab_title, url) {
+	 chrome.bookmarks.create({
+	 	'parentId': null,
+        'title': tab_title,
+        'url': url};
+        //, kill(tab_Id); 
+}*/
+	
+
+function testKeys(processes) {
+
+	var x = Object.getOwnPropertyNames(processes);
+	var y = Object.keys(processes);
+
+	for (var i = 0; i < y.length; i++) {
+		console.dir(y[i]);
+	}
+}
+
+
+function getRemainingMemory() {
+	chrome.system.memory.getInfo(function(info) {
+		console.dir("Availably capabity in bytes: "+ info.availableCapacity);
+		console.dir("Total capabity in bytes: "+ info.capacity);
+	});
+}
+
+
+function populateHTML(obj_array) {
+
+	console.dir("Test 1");
+
+	for (var i = 0; i < tabs.length; i++) {
+
+		var liNode = document.createElement("li");
+		console.dir("li created");
+
+		liNode.innerHTML = obj_array[i].tab_title;
+
+		//var liTextNode = document.createTextNode(obj_array[i].tab_title);
+		console.dir("li contents created");
+
+		//liNode.appendChild(liTextNode);
+		//console.dir("text appended to li");
+
+		olNode.appendChild(liNode);
+		console.dir("li appended to ol");
+
+		bodyNode.appendChild(olNode);
+	}	
+	console.dir("Test 2");
+} 
+
+function addToList(obj_array) {
+	liNode.innerHTML = obj_array[i].tab_title;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
